ea1c9bf0b5dabc97f574fce8456b074e1548376e
Alec <ayj.max@gmail.com>
2022-10-04 01:44:39 -0400

    Added rn-perfect-sketch-canvas

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from 'react';
+import { StyleSheet, SafeAreaView, Button } from 'react-native';
+import { SketchCanvas, SketchCanvasRef } from 'rn-perfect-sketch-canvas';
+
+
+//Components
+import Toolbar from './components/toolbar';
+
 
 export default function App() {
+  const canvasRef = useRef(SketchCanvasRef);
+  const handleClick = () => {
+    alert("Reset...");
+    canvasRef.current.reset();
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <SketchCanvas
+        ref={canvasRef}
+        strokeColor={'black'}
+        strokeWidth={8}
+        containerStyle={styles.container}
+      />
+      <Toolbar/>
+      <Button onPress={() => handleClick()} title="Reset" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
