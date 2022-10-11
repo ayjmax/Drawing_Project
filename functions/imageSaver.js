699384e5765353cdce16a737d842dff98a2bea14
Alec <ayj.max@gmail.com>
2022-10-10 21:43:03 -0400

    New image saver

---
+++ functions/imageSaver.js
@@ -0,0 +1,21 @@
+import * as FileSystem from 'expo-file-system';
+import * as Sharing from 'expo-sharing';
+
+async function saveImage (fileName, imageData) {
+  const fileUri = FileSystem.documentDirectory + `${fileName}.png`;
+
+  await FileSystem.writeAsStringAsync(
+    fileUri,
+    imageData,
+    { 'encoding': FileSystem.EncodingType.Base64 }
+  )
+  .then(() => {
+    console.log('Image base64 writing...Success!!!');
+    Sharing.shareAsync(fileUri);  //Give sharing capabilities
+  })
+  .catch((err) => {
+    console.log('ERROR: ', err);
+  });
+}
+
+export default testSaveImage;
