943b75fae481020038e721c2df50bea810fa6b89
Alec <ayj.max@gmail.com>
2022-10-10 21:42:52 -0400

    New header and image saving functionality

--- App.js
+++ App.js
@@ -2,31 +2,38 @@ import React, { useRef, useState } from 'react';
 import { View, SafeAreaView, Button } from 'react-native';
 
 //Components
+import Header from './components/Header/header';
 import Toolbar from './components/Toolbar/toolbar';
 import Canvas from './components/Canvas/canvas';
 
 //Styles
 import styles from './AppStyles';
 
+//Functions
+import saveImage from './functions/imageSaver';
+
 
 export default function App() {
   const canvasRef = useRef(null);
   const [color, setColor] = useState('black');
   const [sWidth, setWidth] = useState(8);
+  let imageData;
 
-  const handleClick = () => {
-    alert("Reset...");
-    canvasRef.current.reset();
+  const handleSave = () => {
+    alert("Saved!");
+    console.log('hello');
+    imageData = canvasRef.current.toImage().encodeToBase64();
+    console.log(typeof(imageData));
+    // saveImage(image, example);
+    saveImage("bruh", imageData);
   }
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.body}>
+      <Header ref={canvasRef} />
       <Canvas color={color} sWidth={sWidth} ref={canvasRef} />
-
-      <View>
-        <Toolbar color={color} setColor={setColor} setWidth={setWidth} />
-        <Button style={styles.button} onPress={() => handleClick()} title="Reset" />
-      </View>
+      <Toolbar color={color} setColor={setColor} sWidth={sWidth} setWidth={setWidth} />
+      <Button onPress={handleSave} color={"#50C878"} title="Save" />
     </SafeAreaView>
   );
 }
